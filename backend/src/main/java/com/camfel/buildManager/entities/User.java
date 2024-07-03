546adf14f4fcbc8f1b126bb4dca1ba058892c6da
rodrigoschlichta <rodrigo_schlichta@hotmail.com>
2024-07-03 19:29:13 -0300

    User Commit

--- backend/src/main/java/com/camfel/buildManager/entities/User.java
+++ backend/src/main/java/com/camfel/buildManager/entities/User.java
@@ -1,6 +1,71 @@
 package com.camfel.buildManager.entities;
 
-public class User {
+import java.io.Serializable;
+import java.util.Objects;
+
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String name;
+    private String lastName;
+    private String password;
+
+    public User() {
+    }
+
+    public User(Long id, String name, String lastName, String password) {
+        this.id = id;
+        this.name = name;
+        this.lastName = lastName;
+        this.password = password;
+    }
 
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(getId(), user.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getId());
+    }
 }
+
+
