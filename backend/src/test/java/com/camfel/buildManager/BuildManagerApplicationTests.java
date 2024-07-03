aa134ada73365c9e2a99a65fa62c0e877c79e678
rodrigoschlichta <rodrigo_schlichta@hotmail.com>
2024-07-03 18:18:46 -0300

    First Commit

---
+++ backend/src/test/java/com/camfel/buildManager/BuildManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.camfel.buildManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
