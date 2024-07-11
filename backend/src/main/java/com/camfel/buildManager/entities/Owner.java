3b19ec88d9bf4b1bab6bf6fe37b75f29bac97d06
rodrigoschlichta <rodrigo_schlichta@hotmail.com>
2024-07-11 15:44:53 -0300

    getters and setters fix

--- backend/src/main/java/com/camfel/buildManager/entities/Owner.java
+++ backend/src/main/java/com/camfel/buildManager/entities/Owner.java
@@ -28,34 +28,42 @@ public class Owner implements Serializable {
     }
 
     public String getName() {
+
         return name;
     }
 
     public void setName(String name) {
+
         this.name = name;
     }
 
     public Long getCpf() {
+
         return id;
     }
 
     public void setCpf(Long id) {
+
         this.id = id;
     }
 
     public String getPhone() {
+
         return phone;
     }
 
     public void setPhone(String phone) {
+
         this.phone = phone;
     }
 
     public String getEmail() {
+
         return email;
     }
 
     public void setEmail(String email) {
+
         this.email = email;
     }
 
--- backend/src/main/java/com/camfel/buildManager/entities/User.java
+++ backend/src/main/java/com/camfel/buildManager/entities/User.java
@@ -32,34 +32,42 @@ public class User implements Serializable {
 
 
     public Long getId() {
+
         return id;
     }
 
     public void setId(Long id) {
+
         this.id = id;
     }
 
     public String getName() {
+
         return name;
     }
 
     public void setName(String name) {
+
         this.name = name;
     }
 
     public String getLastName() {
+
         return lastName;
     }
 
     public void setLastName(String lastName) {
+
         this.lastName = lastName;
     }
 
     public String getPassword() {
+
         return password;
     }
 
     public void setPassword(String password) {
+
         this.password = password;
     }
 
